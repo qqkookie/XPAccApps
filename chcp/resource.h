829688cf46bee33dfc8af436cf89f1663cef2633
qqkookie <39845532+qqkookie@users.noreply.github.com>
2023-08-04 11:34:39 +0900

    Need fixing

--- chcp/resource.h
+++ chcp/resource.h
@@ -1,25 +1,10 @@
-/*
- * Copyright 2021 Hugh McMaster
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
- */
+#pragma once
 
-#include <windef.h>
+#define RC_STRING_MAX_SIZE 3072
 
-/* Translation IDs */
+#define STRING_ERROR_INVALID_PARAM_FORMAT 107
 
-#define STRING_ACTIVE_CODE_PAGE   100
-#define STRING_INVALID_CODE_PAGE  101
-#define STRING_USAGE              102
+#define STRING_CHCP_ERROR1         302
+#define STRING_CHCP_ERROR4         305
+
+#define STRING_CHCP_HELP   605
